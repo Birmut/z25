c00901da62736534843b43580079ba5a9865e3dc
Anton Borisov <vip.borisov228@mail.ru>
2020-02-21 01:02:23 +0300

    lesson 9 borisov

--- lesson9/homework.py
+++ lesson9/homework.py
@@ -13,3 +13,61 @@ https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
 - выдать сообщение об ошибке, если в полный стек добавить элемент нельзя,
 или из пустого стека достать элемент
 """
+
+class Queue:
+    def __init__(self, size = None):
+        self.size = size
+        self.count = []
+
+        def check(self):
+         if self.size > len(self.count):
+             return True
+            else:
+             return False
+
+        def printer(self):
+                print("size is {self.size} , count is {count.size}".format)
+
+        def add(self,obj):
+            if check is True:
+                self.count.append(obj)
+            else:
+                raise Exception('smth with obj')
+
+class Stack:
+    def __init__(self, size = None):
+        self.size = size
+        self.value = []
+
+        def printer(self):
+            print("size is{}, steck is{}".format(self.size,self.value))
+
+            def check(self):
+                if self.size > len(self.value):
+                    return True
+                else:
+                    return False
+
+            def add(self, obj):
+                if check is True:
+                    self.value.append(obj)
+                else:
+                    return False
+
+                def get(self):
+                    if self.value == 0:
+                        raise Exception("empty")
+                    else:
+                        del self.__value[-1]
+                    t = self.value[-1]
+                    return t
+
+
+
+
+
+
+
+
+
+
