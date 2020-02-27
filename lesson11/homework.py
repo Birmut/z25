13ef5ce59fd4390f812de00ab412e52ae143a8ec
Anton Borisov <vip.borisov228@mail.ru>
2020-02-27 23:51:53 +0300

    lesson11 borisov

--- lesson11/homework.py
+++ lesson11/homework.py
@@ -5,6 +5,25 @@ n включительно.
 """
 
 
+class Fibonacci:
+    def __init__(self, n):
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.fib1 = 1
+        self.fib2 = 1
+        if self.n == 1 or self.n == 0:
+            return 1
+        else:
+            for i in range(2, n):
+                fib1, fib2 = self.fib2, self.fib1 + self.fib2
+                return fib2
+        
+
+
 """
 2.
 Напишите класс, объектом которого будет итератор производящий только
@@ -12,11 +31,44 @@ n включительно.
 """
 
 
+class Even:
+    def __init__(self, n, start):
+        n.self = n
+        self.start = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.start <= self.n:
+            return self.start + 2
+        else:
+            raise StopIteration
+
+
 """
 3.
 Напишите итератор factorials(n), генерирующий последовательность
 факториалов натуральных чисел.
 """
+
+
+class Factorial:
+    def __init__(self, n):
+        self.n = n
+        self.start = 1
+        self.result = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.start <= self.n:
+            self.result *= self.start
+            self.start += 1
+            return self.result
+        else:
+            raise StopIteration
 
 
 """
--- lesson11/tests.py
+++ lesson11/tests.py
@@ -1,9 +1,28 @@
 import unittest
+from homework import Fibonacci, Even, Factorial
 
 
 class HomeworkTest(unittest.TestCase):
     def test_ok(self):
         self.assertTrue(True)
+
+    def Fibonacci_test(self):
+        _list = list(Fibonacci(8))
+        self.assertEqual(_list, [0, 1, 1, 2, 3, 5, 8, 13])
+        _list = list(Fibonacci(10))
+        self.assertEqual(_list, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+
+    def Even_test(self):
+        _list = list(Even(10))
+        self.assertEqual(_list, [2, 4, 6, 8, 10])
+        _list = list(Even(12))
+        self.assertEqual(_list, [2, 4, 6, 8, 10, 12])
+
+    def Factorial_test (self):
+        _list = list(Factorial(6))
+        self.assertEqual(obj, [1, 2, 6, 24, 120, 720])
+        _list= list(Factorial(9))
+        self.assertEqual(obj, [1, 2, 6, 24, 120, 720, 5040, 40320, 362880])
 
 
 if __name__ == '__main__':
